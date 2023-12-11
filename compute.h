f47c9473090be096add5e3afefa63a49e3cb673c
westa3 <111154660+westa3@users.noreply.github.com>
2023-12-11 11:27:40 -0500

    Update compute.h

--- compute.h
+++ compute.h
@@ -1,1 +1,18 @@
-void compute();
+#ifndef COMPUTE_H
+#define COMPUTE_H
+
+#include "vector.h"
+
+void initDeviceMemory(int numEntities);
+void freeDeviceMemory();
+void copyHostToDevice();
+void copyDeviceToHost();
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    void compute();
+    #ifdef __cplusplus
+}
+#endif
+
+#endif // COMPUTE_H
